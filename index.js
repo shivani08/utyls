d5eca28a24a0b9e8bd526a0656f73e589a235a41
sashank6 <sashank.alladi@live.com>
2016-04-10 11:21:34 -0500

    Added Utility function leftpad

--- index.js
+++ index.js
@@ -2,8 +2,35 @@
 var utyls = (function () {
     function utyls() {
     }
-    utyls.prototype.add = function (first, second) {
-        return first + second;
+    utyls.prototype.leftpad = function (str, len, paddingChar) {
+        if (paddingChar === void 0) { paddingChar = '*'; }
+        str = String(str);
+        var checkValues = true;
+        try {
+            //TypeChecking of individual parameters    
+            if (Number.isInteger(len) === false) {
+                checkValues &= false;
+                throw "Required length is not an integer, please correct the 2nd parameter";
+            }
+            if (this.isChar(paddingChar) === false) {
+                checkValues &= false;
+                throw "Padding character is not a character, please correct 3rd parameter";
+            }
+            if (checkValues) {
+                while (str.length != len)
+                    str = paddingChar + str;
+                return str;
+            }
+            else {
+                return undefined;
+            }
+        }
+        catch (err) {
+            console.log("ERROR: " + err);
+        }
+    };
+    utyls.prototype.isChar = function (charVal) {
+        return charVal.length === 1 && (charVal instanceof String) === false;
     };
     return utyls;
 }());
--- test.js
+++ test.js
@@ -1,3 +1,40 @@
-var utyls=require('./');
+var utyls = require('./');
 
-console.log(utyls.prototype.add(1,2));
+var x = new utyls();
+
+
+testsFailed = 0;
+//Tests for isChar
+
+if (x.isChar(7) !== false)
+    testsFailed++;
+
+if (x.isChar('g') !== true)
+    testsFailed++;
+
+if (x.isChar('ghj') !== false)
+    testsFailed++;
+
+if (x.isChar(7.65) !== false)
+    testsFailed++;
+
+//TODO: Tests for inherited datatypes for isChar
+
+
+//Tests for leftpad
+
+if (x.leftpad("23", 5) !== "***23")
+    testsFailed++;
+
+if (x.leftpad("ck", 4, '#') !== "##ck")
+    testsFailed++;
+
+
+//TODO: Add More Tests
+
+
+
+
+
+
+console.log("Tests Failed: " + testsFailed);
