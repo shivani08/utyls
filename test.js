26cf8ea893cd93cb844c1b6c82ff7ed77197493e
Sashank Alladi <sashank6@users.noreply.github.com>
2016-04-13 14:51:00 -0500

    Update test.js

--- test.js
+++ test.js
@@ -10,13 +10,13 @@ describe('Tests for isChar->',function(){
   
 it('Passes Integer',function(){
     
-   assert.equal(utobj.isChar(7),false,""); 
+   assert.strictEqual(utobj.isChar(7),false,""); 
     
 });
 
 it('Passes Char',function(){
     
-   assert.equal(utobj.isChar('g'),true,"");  
+   assert.strictEqual(utobj.isChar('g'),true,"");  
     
 });
 
@@ -24,7 +24,7 @@ it('Passes Char',function(){
 it('Passes String',function(){
    
    
-   assert.equal(utobj.isChar('string'),false,"")
+   assert.strictEqual(utobj.isChar('string'),false,"")
     
 });
 
@@ -32,7 +32,7 @@ it('Passes String',function(){
 it('Passes float/double',function(){
    
    
-   assert.equal(utobj.isChar(5.67),false,"");
+   assert.strictEqual(utobj.isChar(5.67),false,"");
     
 });
 });
@@ -46,27 +46,27 @@ describe("Tests for isInteger->",function(){
    it("Passes char",function(){
       
       
-      assert.equal(utobj.isInteger('7'),false,"");
+      assert.strictEqual(utobj.isInteger('7'),false,"");
        
    });
    
    
    it("Passes String",function(){
       
-      assert.equal(utobj.isInteger("787"),false,"");
+      assert.strictEqual(utobj.isInteger("787"),false,"");
        
    });
    
    
    it("Passes double/float",function(){
       
-      assert.equal(utobj.isInteger(6.78),false,"");
+      assert.strictEqual(utobj.isInteger(6.78),false,"");
        
    });
    
    it("Passes Integer",function(){
       
-      assert.equal(utobj.isInteger(7),true,"");
+      assert.strictEqual(utobj.isInteger(7),true,"");
        
    });
    
@@ -85,64 +85,64 @@ describe("Tests for leftpad",function(){
    
    it("Without default arguement",function(){
       
-       assert.equal(utobj.leftpad("7",3),"**7","");   
+       assert.strictEqual(utobj.leftpad("7",3),"**7","");   
    });
    
    it("With default arguement",function(){
       
-       assert.equal(utobj.leftpad("7",3,"0"),"007","");
+       assert.strictEqual(utobj.leftpad("7",3,"0"),"007","");
        
    });
    
    it("Passes required length less than string length",function(){
       
-      assert.equal(utobj.leftpad("788",2,"6"),"788","");
+      assert.strictEqual(utobj.leftpad("788",2,"6"),"788","");
        
    });
    
    
    it("Passes a string instead of padding character",function(){
       
-      assert.equal(utobj.leftpad("776",10,"607"),undefined,"") ;
+      assert.strictEqual(utobj.leftpad("776",10,"607"),undefined,"") ;
    });
    
    it("Passes negative length as required  length",function(){
       
-      assert.equal(utobj.leftpad("gha",-9),"gha","");
+      assert.strictEqual(utobj.leftpad("gha",-9),"gha","");
        
    });
    
    
    it("Passes empty string ",function(){
        
-      assert.equal(utobj.leftpad("",10),"**********",""); 
+      assert.strictEqual(utobj.leftpad("",10),"**********",""); 
    });
    
    it("Passes String and required length",function(){
       
-      assert.equal(utobj.leftpad("ThisisTextoflength50",50),"******************************ThisisTextoflength50",""); 
+      assert.strictEqual(utobj.leftpad("ThisisTextoflength50",50),"******************************ThisisTextoflength50",""); 
    });
    
    
    it("Passes string and a double value as required length",function(){
     
-      assert.equal(utobj.leftpad("",2.34),undefined,"");
+      assert.strictEqual(utobj.leftpad("",2.34),undefined,"");
     
    });
    
    it("Passes string and a required double value with a padding character",function(){
      
-     assert.equal(utobj.leftpad("",2.34,'0'),undefined,"");
+     assert.strictEqual(utobj.leftpad("",2.34,'0'),undefined,"");
    });
    
    it("Passes string and fraction(when solved integer) as required length",function(){
     
-    assert.equal(utobj.leftpad("",21/7),"***","");
+    assert.strictEqual(utobj.leftpad("",21/7),"***","");
    });
    
    it("Passes string and fraction(when solved double) as required length",function(){
     
-    assert.equal(utobj.leftpad("",22/7),undefined,"");
+    assert.strictEqual(utobj.leftpad("",22/7),undefined,"");
    });
    
    
